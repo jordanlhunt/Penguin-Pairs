ca39d979b2b68edf46e2156e0096d35b87dae5a0
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2024-07-14 21:03:34 -0500

    Add project files.

---
+++ Project1/Engine/AssetManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace Project1.Engine
+{
+
+    internal class AssetManager
+    {
+        #region Member Variables
+        ContentManager contentManager;
+        #endregion
+        #region Properties
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Creates a AssetManager object
+        /// </summary>
+        /// <param name="contentManager"></param>
+        public AssetManager(ContentManager contentManager)
+        {
+            this.contentManager = contentManager;
+        }
+        #endregion
+        #region Public Methods
+        #endregion
+        #region Private Methods
+        #endregion
+    }
+}
