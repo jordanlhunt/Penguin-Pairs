08fc3a4b9af128bdd93a5e3c658d4104772be167
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2024-07-30 11:46:41 -0400

    Added GameState.cs

---
+++ Project1/Engine/GameState.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project1.Engine
+{
+    internal class GameState
+    {
+        #region Member Variables
+        protected GameObjectList gameObjectsList;
+        #endregion
+        #region Properties
+        #endregion
+        #region Constructor
+        protected GameState()
+        {
+            gameObjectsList = new GameObjectList();
+        }
+        #endregion
+        #region Public Methods
+        public virtual void HandleInput(InputHelper inputHelper)
+        {
+            gameObjectsList.HandleInput(inputHelper);
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+            gameObjectsList.Update(gameTime);
+        }
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            gameObjectsList.Draw(gameTime, spriteBatch);
+        }
+        public virtual void Reset()
+        {
+            gameObjectsList.Reset();
+        }
+        #endregion
+        #region Private Methods
+        #endregion
+    }
+}
